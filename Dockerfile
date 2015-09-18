cb41567b608b9750dca1179f5ccc803243b8059e
Pavel Studenik <pstudeni@redhat.com>
2015-09-18 16:53:45 +0200

    register system only when it is not registered

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,18 @@ RUN sed s/enabled=0/enabled=1/g /etc/yum.repos.d/spacewalk-nightly.repo -i && \
     sed s/enabled=1/enabled=0/g /etc/yum.repos.d/spacewalk.repo -i
 
 RUN dnf install spacewalk-proxy-selinux spacewalk-proxy-installer \
-rhn-client-tools rhn-check rhn-setup rhnsd m2crypto wget -y
+rhn-client-tools rhn-check rhn-setup rhnsd m2crypto wget which -y
 
 ENV DOCKER_HOST unix:///tmp/docker.sock
 
 ADD proxyanswers.txt /root/proxyanswers.txt
 ADD rhnclient.py /root/rhnclient.py
+ADD configure.sh /root/configure.sh
 ADD run.sh /root/run.sh
 
 # it needs to registered to spacewalk
 RUN chmod a+x /root/run.sh
+
+EXPOSE 80 443 5222 5269 5280
 
 CMD /root/run.sh
--- run.sh
+++ run.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/bash
 # register.sh <username> <password> <server>
 
-HOSTNAME=$( hostname )
-ANSWERFILE="/root/proxyanswers.txt"
-RHN_SERVER=${3:-$RHN_SERVER}
-RHN_USER=${1:-$RHN_USER}
-RHN_PASS=${2:-$RHN_PASS}
-
-# registration to spacewalk
-wget http://$RHN_SERVER/pub/RHN-ORG-TRUSTED-SSL-CERT -O /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT
-mkdir /root/ssl-build
-
-rhnreg_ks --username=$RHN_USER --serverUrl=https://$RHN_SERVER/XMLRPC --sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT --profilename=client --password=$RHN_PASS --force
-rhn_check -vv
-
-# configure rhn proxy
-sed s/"<hostname>"/$HOSTNAME/g $ANSWERFILE -i
-sed s/"<rhn_server>"/$RHN_SERVER/g $ANSWERFILE -i
-
-wget https://raw.githubusercontent.com/spacewalkproject/spacewalk/master/proxy/installer/get_system_id.xslt -O  /usr/share/rhn/get_system_id.xslt
-SYSTEM_ID=$( xsltproc /usr/share/rhn/get_system_id.xslt /etc/sysconfig/rhn/systemid | sed s/ID-//g )
-
-python /root/rhnclient.py $RHN_USER $RHN_PASS $RHN_SERVER $SYSTEM_ID
-configure-proxy.sh --answer-file=/root/proxyanswers.txt --force --non-interactive
+$( which rhn_check ) -vv || $( which bash ) /root/configure.sh
 
 echo "router .."
 /usr/bin/router -c /etc/jabberd/router.xml &
@@ -37,7 +16,8 @@ echo "s2s .."
 /usr/bin/s2s -c /etc/jabberd/s2s.xml &
 
 echo "httpd .."
-httpd
+$( which httpd )
 
 echo "squid .."
-squid -f /etc/squid/squid.conf
+$( which squid ) -f /etc/squid/squid.conf -z
+$( which squid ) -f /etc/squid/squid.conf -NX -d 2
